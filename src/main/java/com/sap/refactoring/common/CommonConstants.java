7a76443783cd209d2d2d45801000669130b52729
rafaelhua <huarafael@gmail.com>
2026-03-30 23:18:13 -0400

    improve

---
+++ src/main/java/com/sap/refactoring/common/CommonConstants.java
@@ -0,0 +1,8 @@
+package com.sap.refactoring.common;
+
+public final class CommonConstants {
+	public static final String USERS_PATH = "/users";
+	public static final String EMAIL_PATH = "/{email}";
+
+	private CommonConstants() {}
+}
